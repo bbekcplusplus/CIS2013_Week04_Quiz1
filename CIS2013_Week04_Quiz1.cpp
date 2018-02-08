d402cfd9a6568401d6f1694810f98305291a199c
Bibek Gupta <bibek.gupta@okstate.edu>
2018-02-07 18:16:43 -0600

    Initial commit

---
+++ CIS2013_Week04_Quiz1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cout << "How many numbers per line would you like? ";
+	cin >> x;
+
+	for (int i = 0; i < 2000; i++)
+	{
+		if (i % 50== 0) {
+			cout << "FOO" << endl;
+		}
+		if(i % 33 == 0){
+			cout << "BAR" << endl;
+		
+		}
+		else {
+
+			cout << i << endl;
+		}
+	}
+
+	return 0;
+}
