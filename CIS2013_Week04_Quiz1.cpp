c9ba9f9cb7b7a6f22fc8fff3a9e6ffdd1008010a
Bibek Gupta <bibek.gupta@okstate.edu>
2018-02-07 18:58:32 -0600

    CIS2013_Week04_Quiz1
    Changes

--- CIS2013_Week04_Quiz1.cpp
+++ CIS2013_Week04_Quiz1.cpp
@@ -5,21 +5,25 @@ int main() {
 	int x;
 	cout << "How many numbers per line would you like? ";
 	cin >> x;
+	int foo_number = 0;
+	int bar_number = 0;
 
 	for (int i = 1; i < 2000; i++)
 	{
 		if (i % 50== 0) {
 			cout << "FOO" << endl;
+			foo_number++;
 		}
 		if(i % 33 == 0){
 			cout << "BAR" << endl;
-		
+			bar_number++;
 		}
 		else {
 
 			cout << i << endl;
 		}
 	}
-
+	cout << "total FOO is " << foo_number << endl;
+	cout << "total BAR is " << bar_number << endl;
 	return 0;
 }
